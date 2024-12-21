d38b5a6e535df90dbe3d7273bce93945afaf538f
surendra <surendraredddyalamuru01@gmail.com>
2024-12-21 10:44:19 +0530

    VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   for_each = var.vpc
-  source = "https://github.com/surendraalamuru22/tf-vpc"
+  source = "https://github.com/surendraalamuru22/tf-vpc.git"
   cidr_block = each.value.cidr_block
 }
+
