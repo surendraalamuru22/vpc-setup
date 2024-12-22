3c08aabd81058c464826678cb54d82aa58c75187
surendra <surendraredddyalamuru01@gmail.com>
2024-12-22 18:04:38 +0530

    VPC

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,5 +1,6 @@
 vpc = {
   vpc1 = {
     cidr_block = "10.0.0.0/16"
+    additional_vpc_cidr = ["10.100.0.0/16"]
   }
 }
--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "vpc" {
   for_each = var.vpc
   source = "./vendor/modules/vpc"
   cidr_block = each.value.cidr_block
+  additional_vpc_cidr = each.value.additional_vpc_cidr
 }
 
